e3f6f0b5fb939451814be1650741d7e24773b44c
roalvez <ronaldo.andrade266@gmail.com>
2025-05-18 14:51:07 -0300

    fix(rubocop): fix rubocop mistakes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [ :index, :show ]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -10,7 +10,15 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "login" => "sessions#new", as: :login
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy", as: :logout
+
+  namespace :admin do
+    resources :products
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
-  root 'products#index'
+  root "products#index"
 end
